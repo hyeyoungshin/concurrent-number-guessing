6bc5196eac78a14410944fdbab959f9b1c4e8fd2
Hyeyoung Shin <thunderstorm@Hyeyoungs-Mac-mini.local>
2026-02-19 22:26:17 -0700

    add local_multiplayer

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,25 @@
-use rand::prelude::*;
-use im::HashMap;
+mod local_multiplayer;
+mod box_cas;
 
-// Data Definitions
-
-#[derive(PartialEq)]
-enum GameState {
-    InProgress(SecretNumber, HashMap<PlayerId, Guess>), // game is on-going
-    Over(PlayerId), // winner                           // game is over
-}
-
-type SecretNumber = u32;
-type Guess = u32;
-type PlayerId = u32;
-
-struct Action {
-    player_id: PlayerId,
-    guess: u32
-}
-
-const MAX_NUM_TO_GUESS: u32 = 20;
-const NUM_PLAYERS: u32 = 3;
-
-//
-// Game Logic
-//
-
-// Creates a new game with a random secret number
-// add parameter num_players: u32
-fn start_game(num_players: u32) -> GameState {
-    let mut rng = rand::rng();
-    let answer = rng.random_range(0..MAX_NUM_TO_GUESS);
-
-    println!("Guess a number from 0 to {MAX_NUM_TO_GUESS}");
-    
-    GameState::InProgress(answer, HashMap::new())
-}
-
-// Process a player's action and returns the new game state
-fn do_action(st: GameState, a: Action) -> GameState {
-    match st {
-        GameState::Over(_) => st,
-        GameState::InProgress(secret_num, hash) => {
-            if secret_num == a.guess {
-                GameState::Over(a.player_id)
-            } else {
-                let new_hash = hash.update(a.player_id, a.guess);
-                println!("{:?}", new_hash);
-                GameState::InProgress(secret_num, new_hash)
-            }
-        }
-    }
-}
-
-// Prints the message that should be shown to a specific player
-fn state_view(st: &GameState, player_id: &PlayerId) {
-    match st {
-        GameState::Over(winner) => {
-            if winner == player_id {
-                println!("You won! Game over.");
-            } else {
-                println!("Player {player_id} won.");
-            }
-        }, 
-        GameState::InProgress(secret_num, hash) => {
-            let last_guess = hash.get(player_id);
-            match last_guess {
-                Some(guess) => {
-                    if secret_num < guess {
-                        println!("Guess lower.");
-                    } else {
-                        println!("Guess higher");
-                    }
-                }, 
-                None => println!("Guess a number.")
-            }
-        }
-    }
-}
-
-//
-// Input Parsing and Validation
-// 
-
-// Keeps asking for input until a valid number is entered
-// It's used to get 1) player id and 2) guesses
-fn get_valid_input(max: u32) -> u32 {
-  use std::io;
-
-  let mut input = String::new();
-  match io::stdin().read_line(&mut input) {
-    Ok(_) => {
-        let number = input.trim().parse::<u32>();
-        match number {
-            Ok(num) if num < max => num,
-            Ok(_) => {
-                println!("Invalid input. Try again.");
-                get_valid_input(max)
-            },
-            Err(msg) => {
-                println!("{msg}. Try again.");
-                get_valid_input(max)
-            }
-        }
-    },
-    Err(msg) => {
-        println!("{msg}. Try again.");
-        get_valid_input(max)
-    }
-  }
-}
-
-fn is_game_over(st: &GameState) -> bool {
-    match st {
-        GameState::Over(_) => true,
-        _ => false,
-    }
-}
-
-//
-// Main Game Loop
-//
-
-fn local_game() {
-  // Inner function that repeatedly processes turns
-  fn game_loop (st: GameState) {
-    // 1. Ask which player is taking a turn
-    println!("which player");
-    let player_id = get_valid_input(NUM_PLAYERS);
-    // 2. Show this player the current state
-    state_view(&st, &player_id);
-
-    if !is_game_over(&st) {
-        let a = Action { player_id, guess: get_valid_input(MAX_NUM_TO_GUESS) };
-        let new_st = do_action(st, a);
-        game_loop(new_st)
-    }
-  }
-
-  game_loop(start_game(NUM_PLAYERS))
-}
+// use crate::local_multiplayer::local_game;
 
 fn main() {
-  local_game()
-    
+    use std::net::TcpListener;
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // a single stream represents an open connection between the client and the server
+    // connection is the name for the full request and response process in which 
+    // 1. a client connects to the server
+    // 2. the server generates a response, and 
+    // 3. the server closes the connection
+    for stream in listener.incoming() {
+        // unwrap to panic in case stream has errors
+        // when a client connects to the server we are not actually iterating over connections
+        // instead, we're iterating over "connection attempts"
+        // connection might not be successful for a number of reasons, many of them operating system specific
+        // e.g. many OS have a limit to the number of simultaneous open connections they can support
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    } 
 }
